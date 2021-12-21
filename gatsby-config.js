79ff93738078914a4360c51240679b5bd103b936
Max Blancarte <blancarte@gmail.com>
2021-12-21 08:36:58 -0800

    feat: adding tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "aumentapacientes",
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: 'https://www.aumentapacinetes.com',
+		title: 'aumentapacientes',
+	},
+	plugins: [
+		'gatsby-plugin-image',
+		'gatsby-plugin-postcss',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+	],
 };
