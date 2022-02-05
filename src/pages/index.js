b2d7220dc3cb8bfe67012c0a5c6de4b10cb4f0a9
Max Blancarte <blancarte@gmail.com>
2022-02-04 17:52:34 -0800

    feat: stylelint + custom colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ const IndexPage = () => {
 	return (
 		<div className="flex items-center justify-center h-screen">
 			<h1 className="text-2xl text-purple-700">Proximamente!</h1>
+			<h1 className="bg-secondary text-white">Aumenta pacientes</h1>
+			<h2 className="bg-primary text-white">Aumenta Pacientes</h2>
 		</div>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#2b4570',
+				secondary: '#fbbf0f',
+			},
+		},
 	},
 	plugins: [],
 };
