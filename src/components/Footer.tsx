425f490479bad8fa4a4530281d910cf0590a6d1a
Max Blancarte <blancarte@gmail.com>
2022-02-15 11:52:29 -0800

    fix: CSS Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -60,11 +60,11 @@ export const Footer = () => {
         aprendido de nuestras experiencias y la lo desee, utilizando el enlace
         para des suscribirse que aparecerá en los correos electrónicos.
       </p>
-      <div className="flex flex-col gap-6 mt-16 lg:flex-row lg:justify-between md:gap-0">
+      <div className="flex flex-col gap-6 mt-16 md:flex-row lg:justify-between md:gap-0">
         <p className="mt-5 text-sm font-extrabold leading-4 text-white">
           Legal Terms - Acerca de las politicas de privacidad y uso de cookies
         </p>
-        <p className="px-2 pt-5 pb-12 text-sm font-extrabold text-primary bg-secondary">
+        <p className="px-2 pt-5 pb-12 text-sm font-extrabold text-center text-primary bg-secondary">
           Aumenta Pacientes Derechos Reservados 2022
         </p>
       </div>
