166efe9395403e68e365143e493e193e077b461f
Max Blancarte <blancarte@gmail.com>
2022-02-15 14:29:23 -0800

    fix: update to layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,61 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { StaticImage } from "gatsby-plugin-image";
-import Counter from "./Counter";
-import { useWindowSize } from "../hooks/useWindowSize";
+import React, { useState, useEffect } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import Counter from './Counter';
+import { useWindowSize } from '../hooks/useWindowSize';
 
 interface HeroProps {
-  fechaEvento: string;
-  eventTime: string;
+	fechaEvento: string;
+	eventTime: string;
 }
 
 export const Hero = ({ fechaEvento, eventTime }: HeroProps): JSX.Element => {
-  const { width } = useWindowSize();
-  const [showImage, setShowImage] = useState(true);
+	const { width } = useWindowSize();
+	const [showImage, setShowImage] = useState(true);
 
-  useEffect(() => {
-    if (width < 768) {
-      setShowImage(false);
-    } else {
-      setShowImage(true);
-    }
-  }, [width]);
+	useEffect(() => {
+		if (width < 768) {
+			setShowImage(false);
+		} else {
+			setShowImage(true);
+		}
+	}, [width]);
 
-  return (
-    <div className="pt-8 md:grid px-9 lg:p-6 lg:gap-5 lg:mx-12 md:grid-cols-2 md:justify-items-end md:pt-6 md:gap-5">
-      <div>
-        <StaticImage
-          src="../images/aumenta_pacientes_logo.png"
-          alt="Aumenta Pacientes Logo"
-          className="lg:mx-12"
-        />
-        {showImage && (
-          <StaticImage
-            src="../images/personas_fondo-1.png"
-            alt="Personas Hero"
-            className="lg:block "
-          />
-        )}
-      </div>
-      <div className="lg:place-self-end">
-        <div className="mt-8 text-3xl md:text-3xl lg:text-5xl text-primary ">
-          <h1 className="font-thin">Registrate GRATIS</h1>
+	return (
+		<div className="pt-8 md:grid px-9 lg:p-6 lg:gap-5 lg:mx-12 md:grid-cols-2 md:justify-items-end md:pt-6 md:gap-5">
+			<div>
+				<StaticImage
+					src="../images/aumenta_pacientes_logo.png"
+					alt="Aumenta Pacientes Logo"
+					className="lg:mx-12"
+				/>
+				{showImage && (
+					<StaticImage
+						src="../images/personas_fondo-1.png"
+						alt="Personas Hero"
+						className="lg:block "
+					/>
+				)}
+			</div>
+			<div className="lg:place-self-end">
+				<div className="mt-8 text-3xl md:text-3xl lg:text-5xl text-primary ">
+					<h1 className="font-thin">Regístrate GRATIS</h1>
 
-          <h1 className="font-bold lg:text-3xl text-27">
-            al taller AUMENTA PACIENTES
-          </h1>
-        </div>
-        <div className="my-4 text-base font-light leading-7 md:leading-6 text-primary lg:my-7">
-          <p>
-            Descubre como ofrecer y/o vender tu consulta en internet del
-            <span className="p-2 font-bold bg-secondary">
-              {fechaEvento}
-            </span>{" "}
-          </p>
-        </div>
-        <div className="flex justify-center lg:block">
-          <Counter date={new Date(eventTime)} />
-        </div>
-      </div>
-    </div>
-  );
+					<h1 className="font-bold lg:text-3xl text-27">
+						al taller AUMENTA PACIENTES
+					</h1>
+				</div>
+				<div className="my-4 text-base font-light leading-7 md:leading-6 text-primary lg:my-7">
+					<p>
+						Descubre como ofrecer y/o vender tu consulta en internet del
+						<span className="p-2 font-bold bg-secondary">
+							{fechaEvento}
+						</span>{' '}
+					</p>
+				</div>
+				<div className="md:flex md:justify-center lg:block">
+					<h1 className="font-bold lg:text-3xl text-27 text-primary mb-4">
+						La oportunidad de registro expira en:
+					</h1>
+					<Counter date={new Date(Date.now() + 1500000)} />
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/Tecnicas.tsx
+++ src/components/Tecnicas.tsx
@@ -1,124 +1,140 @@
-import { StaticImage } from "gatsby-plugin-image";
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { StaticImage } from 'gatsby-plugin-image';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export const Tecnicas = () => {
-  return (
-    <>
-      <div className="px-8 mt-16 md:px-20 mb-14">
-        <h1 className="text-2xl font-bold text-primary">¿Que descubrirás</h1>
-        <h1 className="text-2xl font-bold text-primary">
-          en este entrenamiento?
-        </h1>
-      </div>
-      {/* <div className="container mx-auto mb-40 background-image"> */}
-      <div className="hidden mx-auto mb-40 lg:block">
-        <div className="relative flex flex-col gap-4 px-6 mx-20 md:grid md:grid-cols-4 top-14">
-          <div>
-            <StaticImage
-              src="../images/oferta_laptop.png"
-              alt="oferta laptop"
-              className="mb-8"
-            />
-            <p className="font-light text-lightgray">
-              Como eliminar las excusas y creencias que nos auto sabotean para
-              conseguir pacientes.
-            </p>
-          </div>
-          <div>
-            <StaticImage
-              src="../images/oferta_equipo.png"
-              alt="oferta equipo"
-              className="mb-8"
-            />
-            <p className="font-light text-lightgray">
-              Cuales son las 3 áreas en las que nos conviene poner atención para
-              lograr tener la cantidad de pacientes que deseamos.
-            </p>
-          </div>
-          <div>
-            <StaticImage
-              src="../images/oferta_personas.png"
-              alt="oferta equipo"
-              className="mb-8"
-            />
-            <p className="font-light text-lightgray">
-              Cuales son los 3 secretos de los profesionales que tienen muchos
-              pacientes.
-            </p>
-          </div>
-          <div>
-            <StaticImage
-              src="../images/oferta_crecimiento_desktop.png"
-              alt="oferta personas"
-              className="mb-8"
-            />
-            <p className="font-light text-lightgray">
-              Cuales son los 7 errores mas comunes que provocan que no crezca
-              nuestra lista de pacientes.
-            </p>
-          </div>
-        </div>
-      </div>
-      <Carousel
-        className="lg:hidden"
-        autoPlay={true}
-        infiniteLoop={true}
-        showStatus={false}
-        showIndicators={false}
-        showThumbs={false}
-      >
-        <div>
-          <StaticImage
-            src="../images/oferta_laptop.png"
-            alt="oferta laptop"
-            className="mb-8"
-          />
-          <p className="px-12 font-light text-lightgray lg:px-0 ">
-            Como eliminar las excusas y creencias que nos auto sabotean para
-            conseguir pacientes.
-          </p>
-        </div>
-        <div>
-          <StaticImage
-            src="../images/oferta_equipo.png"
-            alt="oferta equipo"
-            className="mb-8"
-            placeholder="blurred"
-          />
-          <p className="px-12 font-light text-lightgray lg:px-0">
-            Cuales son las 3 áreas en las que nos conviene poner atención para
-            lograr tener la cantidad de pacientes que deseamos.
-          </p>
-        </div>
-        <div>
-          <StaticImage
-            src="../images/oferta_personas.png"
-            alt="oferta equipo"
-            className="mb-8"
-            loading="eager"
-            placeholder="blurred"
-          />
-          <p className="px-12 font-light text-lightgray lg:px-0">
-            Cuales son los 3 secretos de los profesionales que tienen muchos
-            pacientes.
-          </p>
-        </div>
-        <div>
-          <StaticImage
-            src="../images/oferta_crecimiento_desktop.png"
-            alt="oferta personas"
-            className="mb-8"
-            loading="eager"
-            placeholder="blurred"
-          />
-          <p className="px-12 font-light lg:px-0 text-lightgray">
-            Cuales son los 7 errores mas comunes que provocan que no crezca
-            nuestra lista de pacientes.
-          </p>
-        </div>
-      </Carousel>
-    </>
-  );
+	return (
+		<>
+			<div className="px-8 mt-16 md:px-20 mb-14">
+				<h1 className="text-2xl font-bold text-primary">¿Que descubrirás</h1>
+				<h1 className="text-2xl font-bold text-primary">en este taller?</h1>
+			</div>
+			{/* <div className="container mx-auto mb-40 background-image"> */}
+			<div className="hidden mx-auto mb-40 lg:block">
+				<div className="relative flex flex-col gap-4 px-6 mx-20 md:grid md:grid-cols-4 top-14">
+					<div>
+						<StaticImage
+							src="../images/oferta_laptop.png"
+							alt="oferta laptop"
+							className="mb-8"
+						/>
+						<div className="font-light text-lightgray">
+							<p>Clase 1</p>
+							<p className="font-bold">LA OPORTUNIDAD</p>
+							<p>
+								Descubrirás como esta era digital te permite llegar con tu
+								servicio de consulta a cientos de pacientes alrededor del mundo.
+							</p>
+						</div>
+					</div>
+					<div>
+						<StaticImage
+							src="../images/oferta_equipo.png"
+							alt="oferta equipo"
+							className="mb-8"
+						/>
+						<div className="font-light text-lightgray">
+							<p>Clase 2</p>
+							<p className="font-bold">EL MECANISMO</p>
+							<p>
+								Aprenderás el mecanismo con el que puedes vivir de tu profesión
+								y aumentar tus ingresos.
+							</p>
+						</div>
+					</div>
+					<div>
+						<StaticImage
+							src="../images/oferta_personas.png"
+							alt="oferta equipo"
+							className="mb-8"
+						/>
+						<div className="font-light text-lightgray">
+							<p>Clase 3</p>
+							<p className="font-bold">EL MAPA</p>
+							<p>
+								Conocerás los elementos que debes tener para conseguir pacientes
+								a través de internet.
+							</p>
+						</div>
+					</div>
+					<div>
+						<StaticImage
+							src="../images/oferta_crecimiento_desktop.png"
+							alt="oferta personas"
+							className="mb-8"
+						/>
+						<div className="font-light text-lightgray">
+							<p>Clase 4</p>
+							<p className="font-bold">LA ESTRATEGIA</p>
+							<p>
+								Compartiré contigo las piezas de la estrategia que te sirven
+								para incrementar tu consulta.
+							</p>
+							Cuales son los 7 errores mas comunes que provocan que no crezca
+							nuestra lista de pacientes.
+						</div>
+					</div>
+				</div>
+			</div>
+			<Carousel
+				className="lg:hidden"
+				autoPlay={true}
+				infiniteLoop={true}
+				showStatus={false}
+				showIndicators={false}
+				showThumbs={false}
+			>
+				<div>
+					<StaticImage
+						src="../images/oferta_laptop.png"
+						alt="oferta laptop"
+						className="mb-8"
+					/>
+					<p className="px-12 font-light text-lightgray lg:px-0 ">
+						Como eliminar las excusas y creencias que nos auto sabotean para
+						conseguir pacientes.
+					</p>
+				</div>
+				<div>
+					<StaticImage
+						src="../images/oferta_equipo.png"
+						alt="oferta equipo"
+						className="mb-8"
+						placeholder="blurred"
+					/>
+					<p className="px-12 font-light text-lightgray lg:px-0">
+						Cuales son las 3 áreas en las que nos conviene poner atención para
+						lograr tener la cantidad de pacientes que deseamos.
+					</p>
+				</div>
+				<div>
+					<StaticImage
+						src="../images/oferta_personas.png"
+						alt="oferta equipo"
+						className="mb-8"
+						loading="eager"
+						placeholder="blurred"
+					/>
+					<p className="px-12 font-light text-lightgray lg:px-0">
+						Cuales son los 3 secretos de los profesionales que tienen muchos
+						pacientes.
+					</p>
+				</div>
+				<div>
+					<StaticImage
+						src="../images/oferta_crecimiento_desktop.png"
+						alt="oferta personas"
+						className="mb-8"
+						loading="eager"
+						placeholder="blurred"
+					/>
+					<p className="px-12 font-light lg:px-0 text-lightgray">
+						Cuales son los 7 errores mas comunes que provocan que no crezca
+						nuestra lista de pacientes.
+					</p>
+				</div>
+			</Carousel>
+		</>
+	);
 };
