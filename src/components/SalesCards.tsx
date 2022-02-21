b03b927b21ac67cd94e8be439de2b3abdf576ab4
Max Blancarte <blancarte@gmail.com>
2022-02-20 21:57:59 -0800

    feat: Venta module

--- src/components/SalesCards.tsx
+++ src/components/SalesCards.tsx
@@ -1,36 +1,97 @@
-import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import {
+  GatsbyImage,
+  getImage,
+  StaticImage,
+  withArtDirection,
+} from "gatsby-plugin-image";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useWindowSize } from "../hooks/useWindowSize";
+import Counter from "./Counter";
 import { PriceCard } from "./PriceCard";
 
 export const SalesCards = () => {
+  const { desktopImage, tabletImage } = useStaticQuery(
+    graphql`
+      query {
+        desktopImage: file(relativePath: { eq: "bere_garantia_desktop.png" }) {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+          }
+        }
+      }
+    `
+  );
+
+  const images = getImage(desktopImage);
+
+  const { width } = useWindowSize();
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (width <= 863) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [width]);
+
   return (
-    <div className="flex flex-col gap-4 mt-8 md:flex-row md:justify-center ">
-      <PriceCard
-        arrow={true}
-        title="PLAN ÚNICO"
-        text="Ahorra 85 dólares"
-        payTerm="*Realiza un solo pago"
-        cost="$497 USD"
-        buttonStyle="bg-secondary"
-        url="https://pay.hotmart.com/W42168207B?off=vjph7fq8"
-      />
-      <PriceCard
-        arrow={false}
-        title="PLAN PRACTICO"
-        text="2 pagos iguales"
-        payTerm="*Primer pago hoy y el segundo el proximo mes"
-        cost="$261.03 USD"
-        buttonStyle="bg-secondary"
-        url="https://pay.hotmart.com/W42168207B?off=zeiwzgpe"
-      />
-      <PriceCard
-        arrow={false}
-        title="PLAN FLEXIBLE"
-        text="3 pagos iguales"
-        payTerm="*Paga a tu ritmo en 3 pagos mensuales"
-        cost="$178.35 USD"
-        buttonStyle="bg-secondary"
-        url="https://pay.hotmart.com/W42168207B?off=j6xxy1dq"
-      />
+    <div>
+      <div className="flex flex-col gap-4 mt-8 md:flex-row md:justify-center">
+        <PriceCard
+          arrow={true}
+          title="PLAN ÚNICO"
+          text="Ahorra 85 dólares"
+          payTerm="*Realiza un solo pago"
+          cost="$497 USD"
+          buttonStyle="bg-secondary"
+          url="https://pay.hotmart.com/W42168207B?off=vjph7fq8"
+        />
+        <PriceCard
+          arrow={false}
+          title="PLAN PRACTICO"
+          text="2 pagos iguales"
+          payTerm="*Primer pago hoy y el segundo el proximo mes"
+          cost="$261.03 USD"
+          buttonStyle="bg-secondary"
+          url="https://pay.hotmart.com/W42168207B?off=zeiwzgpe"
+        />
+        <PriceCard
+          arrow={false}
+          title="PLAN FLEXIBLE"
+          text="3 pagos iguales"
+          payTerm="*Paga a tu ritmo en 3 pagos mensuales"
+          cost="$178.35 USD"
+          buttonStyle="bg-secondary"
+          url="https://pay.hotmart.com/W42168207B?off=j6xxy1dq"
+        />
+      </div>
+      <div className="flex flex-col md:grid md:grid-cols-2 mt-9 ">
+        {isMobile ? (
+          <div className="flex flex-col items-center">
+            <StaticImage
+              src="../images/bere_garantia_mobile.png"
+              alt="oferta laptop"
+            />
+            <Counter date={Date.now() + 100000} />
+          </div>
+        ) : (
+          <div className="flex flex-col items-center self-end gap-16">
+            <StaticImage
+              src="../images/logo_garantia_desktop.png"
+              alt="oferta laptop"
+              className=""
+            />
+            <Counter date={Date.now() + 100000} />
+          </div>
+        )}
+        {!isMobile && (
+          <GatsbyImage image={images} alt="foto anfitrion" className="px-4" />
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Venta.tsx
+++ src/components/Venta.tsx
@@ -1,9 +1,10 @@
 import { StaticImage } from "gatsby-plugin-image";
-import React from "react";
+import React, { useRef } from "react";
 import ReactPlayer from "react-player";
 import { SalesCards } from "./SalesCards";
 
 const Venta = () => {
+  const unirme = useRef<null | HTMLDivElement>(null);
   return (
     <>
       <div className="mx-14">
@@ -61,7 +62,7 @@ const Venta = () => {
             <p className="font-bold">Valorado en $47 dólares</p>
           </div>
         </div>
-        <div>
+        <div ref={unirme}>
           <h1 className="mt-16 text-xl font-bold text-primary">
             Elige el plan que mejor se adapte a tí
           </h1>
@@ -80,39 +81,109 @@ const Venta = () => {
               className=""
             />
             <div className="grid mt-10 font-normal gap-7 text-lightgray">
-              <p className="text-lg ">
-                <span className="font-bold text-primary">
-                  PROGRAMA INTENSIVO DE 12 SEMANAS 100% ONLINE
-                </span>{" "}
-                donde aprenderás como lograr que los pacientes lleguen a ti con
-                la estrategia D.E.P.E. y logres incrementar tu numero de
-                pacientes y multiplicar tus ingresos.
-              </p>
-              <p className="font-bold text-primary">Valorado en $997 dólares</p>
-            </div>
-            <div>
-              <p>
-                {" "}
-                <span className="text-bold">
-                  6 MODULOS DE TRABAJO CON LECCIONES DE VIDEO EN HD{" "}
-                </span>{" "}
-                donde siguiendo la estrategia D.E.P.E. avanzarás paso a paso de
-                forma ordenada, enfocada, pero sobre todo a tu ritmo por que tu
-                eliges tus propios horarios para ver las lecciones que ya
-                témenos pregrabadas y lo puedes hacer desde tu computadora o
-                desde tu celular.
-              </p>
-            </div>
+              <div>
+                <p className="text-lg ">
+                  <span className="font-bold text-primary">
+                    PROGRAMA INTENSIVO DE 12 SEMANAS 100% ONLINE
+                  </span>{" "}
+                  donde aprenderás como lograr que los pacientes lleguen a ti
+                  con la estrategia D.E.P.E. y logres incrementar tu numero de
+                  pacientes y multiplicar tus ingresos.
+                </p>
+                <p className="font-bold text-primary">
+                  Valorado en $997 dólares
+                </p>
+              </div>
 
-            <div>
-              <p>
-                <span className="font-bold text-primary">
-                  ACCESO POR TODO UN AǸO AL PORTAL ONLINE
-                </span>{" "}
-                para que así puedas ver y repetir las lecciones luego de
-                concluir el programa las veces que tu preﬁeras.
-              </p>
-              <p className="font-bold text-primary">Valorado en $497 dólares</p>
+              <div>
+                <p>
+                  {" "}
+                  <span className="text-bold">
+                    6 MODULOS DE TRABAJO CON LECCIONES DE VIDEO EN HD{" "}
+                  </span>{" "}
+                  donde siguiendo la estrategia D.E.P.E. avanzarás paso a paso
+                  de forma ordenada, enfocada, pero sobre todo a tu ritmo por
+                  que tu eliges tus propios horarios para ver las lecciones que
+                  ya témenos pregrabadas y lo puedes hacer desde tu computadora
+                  o desde tu celular.
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span className="font-bold text-primary">
+                    ACCESO POR TODO UN AǸO AL PORTAL ONLINE
+                  </span>{" "}
+                  para que así puedas ver y repetir las lecciones luego de
+                  concluir el programa las veces que tu preﬁeras.
+                </p>
+                <p className="font-bold text-primary">
+                  Valorado en $497 dólares
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  <span className="font-bold text-primary">
+                    PERTENECER DE POR VIDA A LA COMUNIDAD INTERNACIONAL DE
+                    EXITOSAS Y EXITOSOS VIP
+                  </span>{" "}
+                  alumnos profesionales del entregamiento incrementa tu
+                  consulta, el pertenecer a la comunidad es algo invaluable por
+                  que dentro de ella podrás ir compartiendo los avances que
+                  vayas logrando con tus compañeros, y empiezas a sentir el
+                  empuje de ser parte de una tribu que creme no te va a dejar
+                  sola.
+                </p>
+                <p className="font-bold text-primary">
+                  Valorado en $497 dólares
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span className="font-bold text-primary">
+                    12 MENTORIAS GRUPALES EN VIVO ONLINE
+                  </span>{" "}
+                  con la psicóloga Berenice Bastidas para obtener seguimiento,
+                  responder preguntas, aclarar dudas, obtener feedback de tus
+                  avances y ayudarte a lograr alcanzar tu meta.
+                </p>
+                <p className="font-bold text-primary">
+                  Valorado en $597 dólares
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span className="font-bold text-primary">
+                    PLAN DE ESTUDIOS EN PDF
+                  </span>{" "}
+                  para que sepas exactamente en dónde debes de enfocarte cada
+                  semana, para así lograr avanzar a pasos seguros y que así ya
+                  nada te detenga triunfar
+                </p>
+                <p className="font-bold text-primary">
+                  Valorado en $47 dólares
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span className="font-bold text-primary">
+                    METAS SEMANALES DE TRABAJO
+                  </span>{" "}
+                  que aceleraran tus resultados para que nada quede en teoría,
+                  cada lección te lleva a la acción con ejercicios detallados,
+                  de esta manera a medida que avanzas en el programa, también
+                  avanzas en tu negocio.
+                </p>
+                <p className="font-bold text-primary">Falta Cantidad</p>
+              </div>
+              <button
+                className="px-12 py-4 text-base font-bold uppercase transition-transform transform border-2 md:px-20 bg-secondary text-primary border-primary active:scale-75 hover:bg-secondarylight"
+                onClick={() =>
+                  unirme.current.scrollIntoView({ behavior: "smooth" })
+                }
+              >
+                ¡Quiero Unirme!
+              </button>
             </div>
           </div>
         </div>
