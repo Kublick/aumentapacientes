8c4fb9076ae9217446274f1e6fa309f1f5442831
Max Blancarte <blancarte@gmail.com>
2022-02-24 14:13:48 -0800

    feat: s1 css adjustments

--- src/pages/s1.tsx
+++ src/pages/s1.tsx
@@ -25,47 +25,49 @@ export const S1 = () => {
             className="lg:ml-20"
           />
         </div>
-        <h1 className="mt-12 font-light text-primary text-27">
+        <h1 className="mt-12 font-light text-primary text-27 md:w-1/2">
           Tu oportunidad de tener pacientes{" "}
           <span className="font-bold">Desde las redes sociales</span>
         </h1>
-        <div className="mt-10">
+        <div className="mt-10 md:grid md:grid-cols-2 md:gap-4">
           <Videoplayer
             url="https://www.youtube.com/embed/Q_Z-_q-_Zz4"
             controls={false}
           />
-          <h2 className="text-base font-bold mt-7 text-primary">
-            ¿No pudiste tomar nota o tienes duda?
-          </h2>
-          <p className="mt-10 text-base font-light text-primary">
-            No te preocupes! Vuelve a ver el video y revisa tu cuaderno de
-            trabajo. También puedes acceder al grupo de Facebook y Whatsapp.
-          </p>
-
-          <div className="flex flex-col gap-4 mt-7">
-            <button
-              className="w-full py-4 text-base font-bold transition-transform transform border-2 md:px-20 bg-secondary text-primary border-primary active:scale-75 hover:bg-secondarylight"
-              onClick={() => getWorkBook()}
-            >
-              Cuaderno de Trabajo
-            </button>
-            <button
-              className="w-full py-4 text-base font-bold text-white transition-transform transform md:px-20 bg-whatsapp active:scale-75 "
-              onClick={() => whatsappGroup()}
-            >
-              Grupo de Whatsapp
-            </button>
-            <button className="w-full py-4 text-base font-bold uppercase transition-transform transform border-2 md:px-20 bg-secondary text-primary border-primary active:scale-75 hover:bg-secondarylight">
-              No te lo pierdas
-            </button>
-          </div>
-          <div className="my-20">
-            <p className="text-base font-bold text-primary">
-              Agrega un comentario público…
+          <div className="place-self-end">
+            <h2 className="text-base font-bold mt-7 text-primary">
+              ¿No pudiste tomar nota o tienes duda?
+            </h2>
+            <p className="mt-10 text-base font-light text-primary">
+              No te preocupes! Vuelve a ver el video y revisa tu cuaderno de
+              trabajo. También puedes acceder al grupo de Facebook y Whatsapp.
             </p>
           </div>
-          <CommentsFacebook />
         </div>
+
+        <div className="flex flex-col gap-4 md:gap-8 mt-7 md:flex-row">
+          <button
+            className="w-full py-4 text-base font-bold transition-transform transform border-2 bg-secondary text-primary border-primary active:scale-75 hover:bg-secondarylight"
+            onClick={() => getWorkBook()}
+          >
+            Cuaderno de Trabajo
+          </button>
+          <button
+            className="w-full py-4 text-base font-bold text-white transition-transform transform bg-whatsapp active:scale-75 "
+            onClick={() => whatsappGroup()}
+          >
+            Grupo de Whatsapp
+          </button>
+          <button className="w-full py-4 text-base font-bold uppercase transition-transform transform border-2 bg-secondary text-primary border-primary active:scale-75 hover:bg-secondarylight">
+            No te lo pierdas
+          </button>
+        </div>
+        <div className="my-20">
+          <p className="text-base font-bold text-primary">
+            Agrega un comentario público…
+          </p>
+        </div>
+        <CommentsFacebook />
       </div>
     </div>
   );
