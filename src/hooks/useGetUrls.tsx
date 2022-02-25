77072117e8c38f7f320118d0ec6009fe9b8fb40b
Max Blancarte <blancarte@gmail.com>
2022-02-25 07:16:30 -0800

    fix url2

--- src/hooks/useGetUrls.tsx
+++ src/hooks/useGetUrls.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 
 export const useGetUrls = (): any => {
   const [urls, setUrls] = useState([]);
-  const backend = "http://209.145.48.29:4000/links";
 
   useEffect(() => {
     const getUrls = async () => {
       try {
-        const response = await fetch(backend);
+        const response = await fetch("http://209.145.48.29:4000/links");
         const data = await response.json();
         console.log(data[0].whatsappAumenta);
         setUrls(data[0].whatsappAumenta);
