f48733970b7df43b8aa49d3ffe359703170dba2a
Max Blancarte <blancarte@gmail.com>
2022-02-25 07:07:22 -0800

    fix: prepare version

--- src/pages/compra.tsx
+++ src/pages/compra.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Venta from "../components/Venta";
 
 const Compra = () => {
-  return (
-    <div>
-      <Venta />
-    </div>
-  );
+  return <div>{/* <Venta /> */}</div>;
 };
 
 export default Compra;
