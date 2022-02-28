9e563d3e7dff946208312ebca393d23f24f7eb09
Max Blancarte <blancarte@gmail.com>
2022-02-27 20:10:12 -0800

    fix: fixed grid on Testimonios

--- src/components/Testimonios.tsx
+++ src/components/Testimonios.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import Videoplayer from "./Videoplayer";
+import React from 'react';
+import Videoplayer from './Videoplayer';
 
 export const Testimonios = () => {
-  return (
-    <div>
-      <h2 className="mb-8 text-xl font-bold text-center text-primary">
-        Mira las historias inspiradoras, los casos de éxito que ya tomaron
-        acción.
-      </h2>
-      <h1 className="mb-8 text-xl font-bold text-center bg-secondary text-primary">
-        ¿Serás el próximo?
-      </h1>
-      <div className="grid gap-8">
-        <div>
-          <Videoplayer url="https://youtu.be/z9CsFqeleUo" controls={false} />
-          <h1 className="px-4 py-2 text-base font-bold text-center bg-secondary text-primary">
-            Triplicó su retorno de inversión antes de concluir el entrenamiento
-          </h1>
-        </div>
-        <div>
-          <Videoplayer
-            url="https://www.youtube.com/watch?v=DUTbbtn9vJs"
-            controls={false}
-          />
-        </div>
-        <div>
-          <Videoplayer url="https://youtu.be/wXHrOaaJZoM" controls={false} />
-        </div>
-        <div>
-          <Videoplayer url="https://youtu.be/TFc8I4yWV6o" controls={false} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className="mb-8 text-xl font-bold text-center text-primary">
+				Mira las historias inspiradoras, los casos de éxito que ya tomaron
+				acción.
+			</h2>
+			<h1 className="mb-8 text-xl font-bold text-center bg-secondary text-primary">
+				¿Serás el próximo?
+			</h1>
+			<div className="grid md:grid-cols-2 gap-8">
+				<div className="col-span-2">
+					<Videoplayer url="https://youtu.be/z9CsFqeleUo" controls={false} />
+					<h1 className="px-4 py-2 text-base font-bold text-center bg-secondary text-primary">
+						Triplicó su retorno de inversión antes de concluir el entrenamiento
+					</h1>
+				</div>
+				<div>
+					<Videoplayer url="https://youtu.be/NmL3jxP9BB0" controls={false} />
+				</div>
+				<div>
+					<Videoplayer url="https://youtu.be/4G_j_Qk7Gvc" controls={false} />
+				</div>
+				<div>
+					<Videoplayer url="https://youtu.be/wXHrOaaJZoM" controls={false} />
+				</div>
+				<div>
+					<Videoplayer url="https://youtu.be/E_Vtjlfbke4" controls={false} />
+				</div>
+			</div>
+		</div>
+	);
 };
