7829a25b2a18b9c20a08fd3c52ad63c06d67d013
Max Blancarte <blancarte@gmail.com>
2022-03-02 23:24:32 -0800

    fix: env setup

--- src/hooks/useGetUrls.tsx
+++ src/hooks/useGetUrls.tsx
@@ -3,16 +3,15 @@ import { useEffect, useState } from 'react';
 export const useGetUrls = (): any => {
 	const [urls, setUrls] = useState([]);
 
+	const backend = process.env.AWSURL;
+
 	useEffect(() => {
 		const getUrls = async () => {
-			const url = '7YXamvsw6hSppoy-s2cPc';
+			const id = '7YXamvsw6hSppoy-s2cPc';
 
 			try {
-				const response = await fetch(
-					`https://6u4nv7easg.execute-api.us-west-1.amazonaws.com/url/7YXamvsw6hSppoy-s2cPc`,
-				);
+				const response = await fetch(`${process.env.AWSURL}/${id}`);
 				const data = await response.json();
-				console.log('la data', data.body.link);
 				setUrls(data.body.link);
 			} catch (error) {
 				console.log(error);
--- src/hooks/usePostUrl.tsx
+++ src/hooks/usePostUrl.tsx
@@ -1,25 +1,26 @@
 const usePostUrl = ({ group }) => {
-  const updateUrl = async () => {
-    const backend = "http://209.145.48.29:4000/links";
-    let id = "621873e0c4b7aba94dbc3dd9";
-    try {
-      const response = await fetch(`${backend}/${id}`, {
-        method: "PATCH",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          whatsappAumenta: group,
-        }),
-      });
-      return;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  updateUrl();
-  return;
+	const updateUrl = async () => {
+		const backend = process.env.AWSURL;
+		const id = '7YXamvsw6hSppoy-s2cPc';
+		try {
+			await fetch(`${backend}/${id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+
+				body: JSON.stringify({
+					link: group,
+					site: 'aumenta',
+				}),
+			});
+			return;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	updateUrl();
+	return;
 };
 
 export default usePostUrl;
