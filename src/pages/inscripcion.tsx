189a23c80d5f854bbdce1f6b9b615ff2399e36ec
Max Blancarte <blancarte@gmail.com>
2022-06-24 18:05:57 -0700

    allow sale site

--- src/pages/inscripcion.tsx
+++ src/pages/inscripcion.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Venta from '../components/Venta';
 
 const Inscripcion = () => {
-	React.useEffect(() => {
-		window.location.href = 'https://www.incrementatuconsulta.com';
-	}, []);
+	// React.useEffect(() => {
+	// 	window.location.href = 'https://www.incrementatuconsulta.com';
+	// }, []);
 
 	return (
 		<div>
